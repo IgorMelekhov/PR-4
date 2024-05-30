2b042dcc6dbca238bfb464d14124a978c47e5fc6
IgorMelekhov <143938424+IgorMelekhov@users.noreply.github.com>
2024-05-30 17:01:10 +0300

    Add files via upload

---
+++ PR-4/PR-4/Class1.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PR_4
+{
+    class Sort<T>
+    {
+        string result;
+        public Sort(T[] my_array)
+        {
+            result = string.Join(";", Buble(my_array));
+        }
+        public Sort(T[] my_array, int a)
+        {
+            result = string.Join(";", Select(my_array));
+        }
+        public Sort(T[] my_array, int l,int r)
+        {
+            result = string.Join(";", MergeSort(my_array,l,r));
+        }
+        /// <summary>
+        /// Метод сортировки пузырьком
+        /// </summary>
+        T[] Buble(T[] my_array)
+        {
+            for (int i = 0; i < my_array.Length; i++)
+            {
+                for (int j = 0; j < my_array.Length - 1; j++)
+                {
+                    dynamic d_my_arraySort = my_array[j], d_my_arraySort1 = my_array[j + 1];
+                    if (d_my_arraySort > d_my_arraySort1)
+                    {
+                        T temp = my_array[j + 1];
+                        my_array[j + 1] = my_array[j];
+                        my_array[j] = temp;
+                    }
+                }
+            }
+            return my_array;
+        }
+        /// <summary>
+        /// Метод получения значений
+        /// </summary>
+        public string Get()
+        {
+            return result;
+        }
+        /// <summary>
+        /// Метод сортировки выбором
+        /// </summary>
+        T[] Select(T[] my_array)
+        {
+            for (int i = 0; i < my_array.Length - 1; i++)
+            {
+                int min = i;
+                for (int j = i + 1; j < my_array.Length; j++)
+                {
+                    dynamic d_my_arraySortMin = my_array[min], d_my_arraySortJ = my_array[j];
+                    if (d_my_arraySortJ < d_my_arraySortMin)
+                    {
+                        min = j;
+                    }
+                }
+                T temp = my_array[min];
+                my_array[min] = my_array[i];
+                my_array[i] = temp;
+            }
+            return my_array;
+        }
+        /// <summary>
+        /// Метод сортировки слиянием
+        /// </summary>
+        static void Merge(T[] my_array, int l, int m, int r)
+        {
+            int i, j, k;
+
+            int n1 = m - l + 1;
+            int n2 = r - m;
+
+            T[] left1 = new T[n1 + 1];
+            T[] right1 = new T[n2 + 1];
+            dynamic a = my_array, MaxValue = Double.MaxValue, left = left1, right = right1;
+            for (i = 0; i < n1; i++)
+            {
+                left[i] = a[l + i];
+            }
+
+            for (j = 1; j <= n2; j++)
+            {
+                right[j - 1] = a[m + j];
+            }
+            left[n1] = MaxValue;
+            right[n2] = MaxValue;
+
+            i = 0;
+            j = 0;
+
+            for (k = l; k <= r; k++)
+            {
+                if (left[i] < right[j])
+                {
+                    a[k] = left[i];
+                    i +=  1;
+                }
+                else
+                {
+                    a[k] = right[j];
+                    j = j + 1;
+                }
+            }
+        }
+        /// <summary>
+        /// Рекурсивный вызов метода слияния
+        /// </summary>
+        static T[] MergeSort(T[] a, int l, int r)
+        {
+            int q;
+            if (l < r)
+            {
+                q = (l + r) / 2;
+                MergeSort(a, l, q);
+                MergeSort(a, q + 1, r);
+                Merge(a, l, q, r);
+            }
+            return a;
+        }
+    }
+}
---
+++ PR-4/PR-4/Form1.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace PR_4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            array.Validating += array_Validating;
+        }
+        private void array_Validating(object sender, CancelEventArgs e)
+        {
+            if (String.IsNullOrEmpty(array.Text))
+            { errorProvider1.SetError(array, "Поле не может быть пустым!"); }
+            //else if (string[] values = input.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);))
+            //{errorProvider1.SetError(array, "В поле должно быть введено число!");}
+            else
+            { errorProvider1.Clear(); }
+        }
+        private void cleanToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            array.Clear();
+            arrayBuble.Clear();
+            arraySelect.Clear();
+            arrayMerge.Clear();
+        }
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Form2 newF = new Form2();
+            newF.Show();
+        }
+
+        private void Sort_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                bool selectBuble = false;
+                bool selectSelect = false;
+                bool selectMerge=false;
+                if (checkBuble.Checked) selectBuble = true;
+                if (checkSelect.Checked) selectSelect = true;
+                if (checkMerge.Checked) selectMerge = true;
+                string input = array.Text;
+                string[] values = input.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+                if ((Int32.TryParse(values[0], out _)))
+                {
+                    int[] intValue = new int[values.Length];
+                    for (int i = 0; i < values.Length; i++)
+                    {
+                        intValue[i] = Convert.ToInt32(values[i]);
+                    }
+                    if (selectBuble )
+                    {
+                        Sort<int> sort = new Sort<int>(intValue);
+                        arrayBuble.Text = sort.Get();
+                    }
+                    if (selectSelect )
+                    {
+                        int a = 0;
+                        Sort<int> sort = new Sort<int>(intValue, a);
+                        arraySelect.Text = sort.Get();
+                    }
+                    if (selectMerge)
+                    {
+                        int l = 0;
+                        int r = intValue.Length - 1;
+                        Sort<int> sort = new Sort<int>(intValue,l,r);
+                        arrayMerge.Text = sort.Get();
+                    }
+                }
+                else if (double.TryParse(values[0], out _))
+                {
+                    double[] doubleValue = new double[values.Length];
+                    for (int i = 0; i < values.Length; i++)
+                    {
+                        doubleValue[i] = Convert.ToDouble(values[i]);
+                    }
+                    if (selectBuble)
+                    {
+                        Sort<double> sort = new Sort<double>(doubleValue);
+                        arrayBuble.Text = sort.Get();
+                    }
+                    if (selectSelect)
+                    {
+                        int a = 0;
+                        Sort<double> sort = new Sort<double>(doubleValue, a);
+                        arraySelect.Text = sort.Get();
+                    }
+                    if (selectMerge)
+                    {
+                        int l = 0;
+                        int r = doubleValue.Length - 1;
+                        Sort<double> sort = new Sort<double>(doubleValue, l, r);
+                        arrayMerge.Text = sort.Get();
+                    }
+                }
+            }
+            catch { }
+        }
+    }
+}
